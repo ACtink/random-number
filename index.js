6e80ec49ffbd1c462b370939ce0e66c4b703e714
Anuj Chopra <anujchopra100@gmail.com>
2024-04-21 18:02:52 +0530

    first release

---
+++ index.js
@@ -0,0 +1,15 @@
+
+ function getRandomNumber(){
+    const randomNumber = Math.floor(Math.random() * 4000 ) + 1
+
+    return randomNumber
+ }
+
+
+
+
+
+
+module.exports = {
+    getRandomNumber
+}
